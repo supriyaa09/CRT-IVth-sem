9326488ceeffc5773b3b50dc67dd2bd712a768cc
Supriya <supriyaaa.926@gmail.com>
2026-01-07 10:39:12 +0530

    Conditional_Statements

--- IVth-SEM/Python/Practice/M01_Language_and_Programming_Basics/S01/PS04_Conditional_Statements.py
+++ IVth-SEM/Python/Practice/M01_Language_and_Programming_Basics/S01/PS04_Conditional_Statements.py
@@ -3,3 +3,11 @@ if x%2==0:
     print("Even")
 else:
     print("Odd")
+
+a=int(input("Enter first number: "))
+if a>0:
+    print("Positive")
+elif a<0:
+    print("Negative")
+else:
+    print("Zero")
