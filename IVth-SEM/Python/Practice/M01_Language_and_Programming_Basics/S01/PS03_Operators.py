a90dd7aa898dd01949d927b6166e07db1f92f1e9
Supriya <supriyaaa.926@gmail.com>
2026-01-07 10:29:58 +0530

    Conditional_Statements
    CRT

--- IVth-SEM/Python/Practice/M01_Language_and_Programming_Basics/S01/PS03_Operators.py
+++ IVth-SEM/Python/Practice/M01_Language_and_Programming_Basics/S01/PS03_Operators.py
@@ -6,3 +6,10 @@ print(x is z)
 l1=[1,2,3]
 l2=[1,2,3]
 print(l1 is l2)
+print(l1 is not l2)#Rfernce algorithm
+print(x>>2) #right shift
+print(x<<2) #left shift
+a=5
+b=4
+print(a^b) #xor
+print(a|b) #or
