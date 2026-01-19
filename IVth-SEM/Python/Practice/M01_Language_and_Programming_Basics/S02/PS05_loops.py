0d689c4ee8b045db8b6d9a52b5580ad061bedd3c
Supriya <supriyaaa.926@gmail.com>
2026-01-19 09:58:52 +0530

    FizzBuzz

--- IVth-SEM/Python/Practice/M01_Language_and_Programming_Basics/S02/PS05_loops.py
+++ IVth-SEM/Python/Practice/M01_Language_and_Programming_Basics/S02/PS05_loops.py
@@ -4,16 +4,39 @@
    #code
    #updating condition
 #while loop:
-counter=0
+'''counter=0
 while counter<2:
     print("While Loop")
     counter+=1
-
 '''
-Read 2 integers start and stop variables. display all even numbers between start and stop (both inclusive)
+
+
+'''Read 2 integers start and stop variables. display all even numbers between start and stop (both inclusive)
 Input 1 10
-Output 2 4 6 8 10'''
-c=2
-while c<=10:
-        print(c,end=" ")
-        c+=2
+Output 2 4 6 8 10
+'''
+'''start=int(input())
+stop=int(input())
+while start<=stop:
+    if start %2==0:
+        print(start,end=" ")
+    start+=1
+print()'''
+
+
+'''if multiple of 3 print "Fizz"
+if multiple of 5 print "Buzz"  
+if multiple of both 3 and 5 print "FizzBuzz"
+else print the number itself'''
+start=int(input())
+stop=int(input())
+while start<=stop:
+    if start %3 ==0 and start %5==0:
+        print("FizzBuzz",end=" ")
+    elif start %3==0:
+        print("Fizz",end=" ")
+    elif start %5==0:
+        print("Buzz",end=" ")
+    else:
+        print(start,end=" ")
+    start+=1
