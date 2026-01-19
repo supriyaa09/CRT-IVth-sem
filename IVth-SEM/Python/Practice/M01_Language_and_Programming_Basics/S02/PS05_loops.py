bf9d2a61e2095973c0a3483cd6d76a0704f317cd
Supriya <supriyaaa.926@gmail.com>
2026-01-19 10:33:23 +0530

    Fetching elements from list

--- IVth-SEM/Python/Practice/M01_Language_and_Programming_Basics/S02/PS05_loops.py
+++ IVth-SEM/Python/Practice/M01_Language_and_Programming_Basics/S02/PS05_loops.py
@@ -28,7 +28,7 @@ print()'''
 if multiple of 5 print "Buzz"  
 if multiple of both 3 and 5 print "FizzBuzz"
 else print the number itself'''
-start=int(input())
+'''start=int(input())
 stop=int(input())
 while start<=stop:
     if start %3 ==0 and start %5==0:
@@ -40,3 +40,32 @@ while start<=stop:
     else:
         print(start,end=" ")
     start+=1
+'''
+#for loop:
+'''Syntax:
+for i in range(start or(default 0),stop (stop is excluded),step or(default 1)):'''
+#for i in range(0,5,1):
+#    print("Hello World!")
+
+'''start=int(input())
+stop=int(input())
+for i  in range(start,stop+1,1):
+    if i%2==0:
+        print(i,end=" ")'''
+#Display n natural numbers in same line using for loop
+'''n=int(input())
+for i in range(1,n+1,1):
+    print(i,end=" ")
+print()
+'''
+#Display n natural numbers in reverse order in same line using for loop
+'''n=int(input())
+for i in range(n,0,-1):
+    print(i,end=" ")
+'''
+#print even numbers from list using for loop (Fetching elements from list)
+l=[1,4,6,3,9,2]
+for i in range(0,len(l),1):
+    if l[i]%2==0:
+        print(l[i],end=" ")
+
