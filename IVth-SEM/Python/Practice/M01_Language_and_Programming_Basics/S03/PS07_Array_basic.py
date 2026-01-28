48f457146c98eabae8b0b1d6a3c51e1f0d268dbe
Supriya <supriyaaa.926@gmail.com>
2026-01-28 10:37:11 +0530

    NUMPY

--- IVth-SEM/Python/Practice/M01_Language_and_Programming_Basics/S03/PS07_Array_basic.py
+++ IVth-SEM/Python/Practice/M01_Language_and_Programming_Basics/S03/PS07_Array_basic.py
@@ -1,8 +1,18 @@
-'''ARRAY==> multiple values in a single variable
-1-D array==> list '''
-'Create a arr of float type and print the elements of array'
+'''ARRAY= multiple values in a single variable
+1-D array=list '''
+#Create a arr of float type and print the elements of array
+'''
 import array
 a=array.array('f',[34.5,7.5,6.7,5.6])
 print(a,type(a))
 a.append(1.2)
 print(a)
+'''
+import numpy as np
+a1=np.array([1,2,3,4,5])
+a2=np.array([[4,5,6],
+            [7,8,9],
+            [10,11,12]])
+print(a1.shape)
+print(a2.shape)
+print(a2.dtype)
