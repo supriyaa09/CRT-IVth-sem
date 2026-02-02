5bf7c23f5b9d008d91c29f0be16b4ccfda7748bf
Supriya <supriyaaa.926@gmail.com>
2026-02-02 09:27:39 +0530

    Assignments

---
+++ IVth-SEM/Python/Practice/M01_Language_and_Programming_Basics/S04/PS08_Loop_Control_&_Debugging.py
@@ -0,0 +1,13 @@
+'''DEBUGGING IN PYTHON
+bug is an error
+Finding and fixing bugs is called debugging
+Types of errors:
+1SYNTAX ERROR:
+--> missing colon at the end of a statement
+--> misspelled keywords
+---> unmatched parentheses, quotes etc, indentation errors.
+2RUNTIME ERROR:
+--> occur during program execution
+--> division by zero, file not found etc.
+
+'''
