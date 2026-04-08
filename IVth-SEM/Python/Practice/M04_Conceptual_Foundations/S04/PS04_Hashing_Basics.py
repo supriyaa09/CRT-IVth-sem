99f8bb56f2d827468e0a3cd158dfd3c1e231d981
Supriya <supriyaaa.926@gmail.com>
2026-04-08 10:21:21 +0530

    08-04-2026

---
+++ IVth-SEM/Python/Practice/M04_Conceptual_Foundations/S04/PS04_Hashing_Basics.py
@@ -0,0 +1,15 @@
+'''li=[27,32,46,36,48,55]
+dict->immutable->(numeric, string, tuple, frozenset)
+d={}->empty dict'''
+'''d={1:"a",[1,2]:"b"}
+print(d) #-> TypeError: unhashable type: 'list' '''
+
+d={1:'a',2:'b',3:'c',1:'z'}
+print(d) #-> {1:'z', 2:'b', 3:'c'}
+print(d[1]) #-> 'z'
+
+def instruction(nums1,nums2):
+    s1=set(nums1)
+    s2=set(nums2)
+    print(list(s1.intersection(s2)))
+instruction([1,2,2,1],[2,2])
